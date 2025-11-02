f2e0d19552e5aa1c572825593d950f262b25f1e6
Nitr0 <bidintha2006@gmail.com>
2025-11-03 00:02:37 +0530

    Fixed A Small BUG

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,63 +1,3 @@
-#extends CharacterBody2D
-#
-#
-#const Walk_Speed = 300.0
-#const JUMP_VELOCITY = -400.0
-#@export var Run_Speed = 500
-#@export_range(0,1) var acc = 0.1
-#@export_range(0,1) var dec = 0.1
-#@onready var player_animation: AnimatedSprite2D = $PlayerAnimation
-#
-#
-#
-#
-#func _physics_process(delta: float) -> void:
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-#
-#
-	#if Input.is_action_just_pressed("JUMP") and (is_on_floor() or is_on_wall()):
-		#velocity.y = JUMP_VELOCITY
-		#if is_on_wall() and not is_on_floor():
-			#player_animation.play("Wall")
-	#
-	#var speed
-	#if Input.is_action_pressed("RUN") and not is_on_wall():
-		#speed = Run_Speed
-	#else:
-		#speed = Walk_Speed
-	#if is_on_wall() and not is_on_floor():
-		#player_animation.play("Wall")
-#
-	#var direction := Input.get_axis("LEFT", "RIGHT")
-	#if direction < 0:
-		#player_animation.flip_h = true
-	#if direction > 0:
-		#player_animation.flip_h = false
-		#
-	#if is_on_floor():	
-		#if direction == 0:
-			#player_animation.play("Idle")
-		#else:
-			#player_animation.play("Run")
-	#else:
-		#player_animation.play("Jump")
-		#
-	#if direction:
-		#velocity.x = move_toward(velocity.x, speed * direction, speed * acc)
-		#player_animation.play("Run")
-	#else:
-		#velocity.x = move_toward(velocity.x, speed * direction, speed)
-		##player_animation.play("Run")
-#
-	#move_and_slide()
-#
-#
-#
-#
-#
-
-
 extends CharacterBody2D
 
 const Walk_Speed = 150.0
@@ -65,7 +5,7 @@ const JUMP_VELOCITY = -350.0
 @export var Run_Speed = 300
 @export_range(0,1) var acc = 0.1
 @export_range(0,1) var dec = 0.1
-const PUSH_FORCE = 100.0
+const PUSH_FORCE = 120.0
 
 @onready var player_animation: AnimatedSprite2D = $PlayerAnimation
 
